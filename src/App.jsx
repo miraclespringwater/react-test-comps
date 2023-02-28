a22ce42081f9d8f5f8bc08d1509251c5f477ee39
George Colletti Barajas <miracle@dreamsmith.net>
2023-02-28 07:02:14 -0600

    add reusable panel component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
+import { useState } from "react";
 import Dropdown from "./components/Dropdown";
 
 const App = () => {
-  return <Dropdown>{/* <div></div> */}</Dropdown>;
+  const [selection, setSelection] = useState(null);
+
+  const handleSelect = (option) => {
+    setSelection(option);
+  };
+
+  const options = [
+    { label: "Red", value: "red" },
+    { label: "Green", value: "green" },
+    { label: "Blue", value: "blue" },
+  ];
+  return (
+    <Dropdown
+      options={options}
+      value={selection}
+      onChange={handleSelect}
+    />
+  );
 };
 
 export default App;
--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,5 +1,47 @@
-const Dropdown = () => {
-  return <div>Dropdown...</div>;
+import { useState } from "react";
+import { GoChevronDown } from "react-icons/go";
+import Panel from "./Panel";
+
+const Dropdown = ({ options, value, onChange }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleClick = () => {
+    setIsOpen((currentIsOpen) => !currentIsOpen);
+  };
+
+  const handleOptionClick = (option) => {
+    setIsOpen(false);
+    onChange(option);
+  };
+
+  const renderedOptions = options.map((option) => {
+    return (
+      <div
+        className="hover:bg-sky-100 rounded cursor-pointer p-1"
+        onClick={() => handleOptionClick(option)}
+        key={option.value}
+      >
+        {option.label}
+      </div>
+    );
+  });
+
+  return (
+    <div className="w-48 relative">
+      <Panel
+        className="flex justify-between items-center cursor-pointer"
+        onClick={handleClick}
+      >
+        {value?.label || "Select..."}
+        <GoChevronDown className="text-lg" />
+      </Panel>
+      {isOpen && (
+        <Panel className="absolute top-full">
+          {renderedOptions}
+        </Panel>
+      )}
+    </div>
+  );
 };
 
 export default Dropdown;
